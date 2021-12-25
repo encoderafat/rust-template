3737f257a39c1cbaf51650963e707b47d0eae414
Rafat Hussain <rafat.hsn@gmail.com>
2021-12-25 10:33:05 +0530

    initial

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+
+const PUZZLE_NUMBER: u8 = 1;
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    crossword_solution: String,
+}
+
+#[near_bindgen]
+impl Contract {
+    pub fn get_puzzle_number(&self) -> u8 {
+        PUZZLE_NUMBER
+    }
+
+    pub fn set_solution(&mut self, solution: String) {
+        self.crossword_solution = solution;
+    }
+
+    pub fn guess_solution(&mut self, guess: String) {
+        if guess == self.crossword_solution {
+            env::log_str("YOU GUESSED RIGHT");
+        }
+        else {
+            env::log_str("TRY AGAIN");
+        }
+    }
+}
+
+/*
+ * the rest of this file sets up unit tests
+ * to run these, the command will be:
+ * cargo test --package rust-template -- --nocapture
+ * Note: 'rust-template' comes from Cargo.toml's 'name' key
+ */
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::{testing_env, AccountId};
+
+    // part of writing unit tests is setting up a mock context
+    // provide a `predecessor` here, it'll modify the default context
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    // TESTS HERE
+}
